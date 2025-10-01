d63915fa03823872b617cac1b12ea3e67bb670c0
Ansel-Kee <62891187+Ansel-Kee@users.noreply.github.com>
2025-10-01 15:25:13 +0800

    Update Djikstra Heap.py

--- Djikstra Heap.py
+++ Djikstra Heap.py
@@ -5,15 +5,16 @@ class MinHeap:
     def __init__(self):
         self.heap = []
         
-
     def push(self, val):
         self.heap.append(val)
         i = len(self.heap) - 1
         while i > 0 and self.heap[(i - 1) // 2] > self.heap[i]:
             self.heap[i], self.heap[(i - 1) // 2] = self.heap[(i - 1) // 2], self.heap[i]
             i = (i - 1) // 2
+
     def size(self):
         return len(self.heap)
+
     def delete(self, value):
         i = -1
         for j in range(len(self.heap)):
@@ -43,27 +44,13 @@ class MinHeap:
         self.delete(out)
         return out
 
-    def heapify(self, i, n):
-        smallest = i
-        left = 2 * i + 1
-        right = left + 1
-
-        if left < n and self.heap[left] < self.heap[smallest]:
-            smallest = left
-        if right < n and self.heap[right] < self.heap[smallest]:
-            smallest = right
-        if smallest != i:
-            self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
-            self.heapify(smallest, n)
-
-
-def dijkstra(V, adj, src):
+def dijkstra(V, adj):
     pq = MinHeap()
     
     dist = [float('inf')] * V
 
-    pq.push([0, src])
-    dist[src] = 0
+    pq.push([0, 0])
+    dist[0] = 0
 
     while pq.size():
         u = pq.pop()[1]
@@ -79,7 +66,6 @@ def dijkstra(V, adj, src):
 
 
 def test(V, E):
-    src = 0
 
     adj = [[] for _ in range(V)]
     pairs = {i:[j for j in range(V) if j != i] for i in range(V)}
@@ -97,44 +83,60 @@ def test(V, E):
         if len(pairs[end]) == 0:
             pairs.pop(end, None)
     t = time.time()
-    dijkstra(V, adj, src)
+    dijkstra(V, adj)
     t = time.time()-t
     return t
 
-runs = 50
+# runs = 50
 
-v_values = [200, 400, 800, 1600, 3200, 6400, 12800]
-v_times = []
-for i in v_values:
-    avg = 0
-    for _ in range(runs):
-        avg += test(i, i*2)
-    v_times.append(avg/runs)
+# v_values = [200, 400, 800, 1600, 3200, 6400, 12800]
 
-plt.figure()
-plt.plot(v_values, v_times, marker="o")
-plt.xlabel("|V| (n)")
-plt.ylabel("Mean time (s)")
-plt.title("Dijkstra (array min): Time vs |V|")
-plt.legend()
-plt.tight_layout()
-plt.savefig("time vs v (heap).png")
-plt.close()
+# v_values = [200, 400, 800, 1600, 3200, 6400, 12800]
+# v_times = []
+# for i in v_values:
+#     avg = 0
+#     for _ in range(runs):
+#         avg += test(i, i*2)
+#     v_times.append(avg/runs)
 
-e_values = [999, 1500, 2000, 3000, 4000, 6400, 12800]
-e_times = []
-for i in e_values:
-    avg = 0
-    for _ in range(runs):
-        avg += test(1000, i)
-    e_times.append(avg/runs)
+# plt.figure()
+# plt.plot(v_values, v_times, marker="o")
+# plt.xlabel("|V| (n)")
+# plt.ylabel("Mean time (s)")
+# plt.title("Dijkstra (array min): Time vs |V|")
+# plt.legend()
+# plt.tight_layout()
+# plt.savefig("time vs v (heap).png")
+# plt.close()
 
-plt.figure()
-plt.plot(e_values, e_times, marker="o")
-plt.xlabel("|E| (n)")
-plt.ylabel("Mean time (s)")
-plt.title("Dijkstra (array min): Time vs |E|")
-plt.legend()
-plt.tight_layout()
-plt.savefig("time vs e (heap).png")
-plt.close()
+# e_values = [1000, 1500, 2000, 3000, 4000, 6400, 12800, 40000, 50000]
+# e_times = []
+# for i in e_values:
+#     avg = 0
+#     for _ in range(runs):
+#         avg += test(200, i)
+#     e_times.append(avg/runs)
+
+# plt.figure()
+# plt.plot(e_values, e_times, marker="o")
+# plt.xlabel("|E| (n)")
+# plt.ylabel("Mean time (s)")
+# plt.title("Dijkstra (array min): Time vs |E|")
+# plt.legend()
+# plt.tight_layout()
+# plt.savefig("time vs e (heap).png")
+# plt.close()
+
+def constructAdj(edges, V):
+    
+    # adj[u] = list of [v, wt]
+    adj = [[] for _ in range(V)]
+
+    for edge in edges:
+        u, v, wt = edge
+        adj[u].append([v, wt])
+        adj[v].append([u, wt])
+
+    return adj
+edges =[[0, 1, 4], [0, 2, 8], [1, 4, 6], [2, 3, 2], [3, 4, 10]]
+print(dijkstra(5, constructAdj(edges, 5)))
